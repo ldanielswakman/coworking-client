091771cb15493544d1bd1dafb5d3b3546cad7675
L Daniel Swakman <d.swakman@gmail.com>
2017-12-17 21:37:38 +0100

    bugfix

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Config default
 var config = (typeof customConfig !== 'undefined') ? customConfig : {
   env: 'production',
-  apiURL: 'https://coworking.ldaniel.eu/api/',
+  apiURL: 'https://coworking.ldaniel.eu/',
   googleMapsAPIKey: 'AIzaSyBMFotJFUPtOhGds8oklETkMO9knxQvnB0',
 }
 
--- js/scripts.js
+++ js/scripts.js
@@ -687,7 +687,7 @@ var SpacesIndex = Vue.component('spaces-index', {
 // Config default
 var config = (typeof customConfig !== 'undefined') ? customConfig : {
   env: 'production',
-  apiURL: 'https://coworking.ldaniel.eu/api/',
+  apiURL: 'https://coworking.ldaniel.eu/',
   googleMapsAPIKey: 'AIzaSyBMFotJFUPtOhGds8oklETkMO9knxQvnB0',
 }
 
