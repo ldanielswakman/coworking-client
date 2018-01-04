765a46320b794dc5038a1f7fc63da10edd01a8de
L Daniel Swakman <d.swakman@gmail.com>
2018-01-04 16:30:04 +0100

    tweaked carousel

--- js/scripts.js
+++ js/scripts.js
@@ -58,12 +58,10 @@ Vue.component('space-gallery', {
       <router-link to="/" exact class="space-detail__close">&times;</router-link>
 
       <!-- col-xs-6 col-sm-4 -->
-      <carousel>
-        <slide v-for="image, key, index in spaceGallery">
-          <figure>
-            <img v-if="image.large" v-bind:src="image.large" v-bind:alt="spaceName + ' - ' + index" />
-            <img v-if="!image.large" v-bind:src="image.medium" v-bind:alt="spaceName + ' - ' + index" />
-          </figure>
+      <carousel class="carousel" :perPage="1">
+        <slide v-for="image, key, index in spaceGallery" :key="index">
+          <img v-if="image.large" v-bind:src="image.large" v-bind:alt="spaceName + ' - ' + index" />
+          <img v-if="!image.large" v-bind:src="image.medium" v-bind:alt="spaceName + ' - ' + index" />
         </slide>
       </carousel>
 
